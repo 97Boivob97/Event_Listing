a444cb73877e5274f6c98c943e7ce7ddfaf2793b
Fahad Bin Kashem <2019-1-60-071@std.ewubd.edu>
2025-12-30 16:38:59 +0600

    Fix Vercel serverless handler

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import app from "./app.js";
 import connectDB from "./src/config/db.js";
 
 let isConnected = false;
 
-export default async function handler(request, response) {
+export default async function handler(req, res) {
   if (!isConnected) {
     await connectDB();
     isConnected = true;
   }
-  return app(request, response);
+  return app(req, res);
 }
